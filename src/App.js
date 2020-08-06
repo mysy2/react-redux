7bbabecbd67f446c09ac6ab19b0588942231b69e
mysy2 <nobalp@gmail.com>
2020-08-06 13:27:46 +0900

    0806

--- src/App.js
+++ src/App.js
@@ -11,12 +11,8 @@ export default class App extends Component {
         return (
             <div className="App">
                 <h1>ROOT</h1>
-                <AddNumberRoot
-                    onClick={function (size) {
-                        this.setState({ number: this.state.number + size });
-                    }.bind(this)}
-                ></AddNumberRoot>
-                <DisplayNumberRoot number={this.state.number}></DisplayNumberRoot>
+                <AddNumberRoot></AddNumberRoot>
+                <DisplayNumberRoot></DisplayNumberRoot>
             </div>
         );
     }
